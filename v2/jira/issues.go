968921a20a06efadd538f139e67468783270ca2f
Adam Haymond <adam@masteryconnect.com>
2016-01-26 11:44:11 -0700

    Refined custom fields and issue fields

--- v2/jira/issues.go
+++ v2/jira/issues.go
@@ -52,27 +52,40 @@ type Issue struct {
 			Subtask     bool   `json:"subtask,omitempty"`
 			IconURL     string `json:"iconUrl,omitempty"`
 		} `json:"issuetype,omitempty"`
-		Parent           *Issue           `json:"parent,omitempty"`
-		Subtasks         []*Issue         `json:"subtasks,omitempty"`
-		Assignee         *User            `json:"assignee,omitempty"`
-		FixVersions      []*Version       `json:"fixVersions,omitempty"`
-		Labels           []string         `json:"labels,omitempty"`
-		Status           *IssueStatus     `json:"status,omitempty"`
-		Resolution       *IssueResolution `json:"resolution,omitempty"`
-		Created          string           `json:"created,omitempty"`
-		Updated          string           `json:"updated,omitempty"`
-		Creator          *User            `json:"creator,omitempty"`
-		Reporter         *User            `json:"reporter,omitempty"`
-		ResolutionDate   string           `json:"resolutiondate,omitempty"`
-		Project          *Project         `json:"project,omitempty"`
-		Effort           *Effort          `json:"customfield_10602"`
-		Team             *Team            `json:"customfield_10604"`
-		Epic             string           `json:"customfield_10008"`
-		ExternalIssueURL string           `json:"customfield_10025"`
-		ExternalIssueID  string           `json:"customfield_10026"`
-		Priority         *Priority        `json:"priority,omitempty"`
-		Components       []*Component     `json:"components,omitempty"`
-		ChangeLog        *ChangeLog       `json:"changelog,omitempty"`
+		Parent         *Issue           `json:"parent,omitempty"`
+		Subtasks       []*Issue         `json:"subtasks,omitempty"`
+		Assignee       *User            `json:"assignee,omitempty"`
+		FixVersions    []*Version       `json:"fixVersions,omitempty"`
+		Labels         []string         `json:"labels,omitempty"`
+		Status         *IssueStatus     `json:"status,omitempty"`
+		Resolution     *IssueResolution `json:"resolution,omitempty"`
+		Created        string           `json:"created,omitempty"`
+		Updated        string           `json:"updated,omitempty"`
+		Creator        *User            `json:"creator,omitempty"`
+		Reporter       *User            `json:"reporter,omitempty"`
+		ResolutionDate string           `json:"resolutiondate,omitempty"`
+		Project        *Project         `json:"project,omitempty"`
+		Effort         struct {
+			Self  string `json:"self,omitempty"`
+			Value string `json:"value,omitempty"`
+			Id    string `json:"id,omitempty"`
+		} `json:"customfield_10602,omitempty"`
+		Team struct {
+			Self  string `json:"self,omitempty"`
+			Value string `json:"value,omitempty"`
+			Id    string `json:"id,omitempty"`
+		} `json:"customfield_10604,omitempty"`
+		Epic             string `json:"customfield_10008"`
+		ExternalIssueURL string `json:"customfield_10025"`
+		ExternalIssueID  string `json:"customfield_10026"`
+		Priority         struct {
+			Self    string `json:"self,omitempty"`
+			IconURL string `json:"iconUrl,omitempty"`
+			Name    string `json:"name,omitempty"`
+			Id      string `json:"id,omitempty"`
+		} `json:"priority,omitempty"`
+		Components []*Component `json:"components,omitempty"`
+		ChangeLog  *ChangeLog   `json:"changelog,omitempty"`
 	} `json:"fields,omitempty"`
 }
 
